19fbd924ddef4e9e11ca6cf5fba70865aa1b82de
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-16 13:00:41 +0100

    to-do manager works

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+# As a user I can list tasks
+# As a user I can display the details of a task
+# As a user I can add a task in my ToDo list
+# As a user I can edit the details of a task
+# As a user I can remove a task from my ToDo list
+
+
 end
